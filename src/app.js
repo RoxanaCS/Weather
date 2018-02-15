994bd57a8aef66d610c0d7eecd258b02b5f564ea
Tanko <tankodani@gmail.com>
2018-02-15 12:55:19 -0300

    iconos

--- src/app.js
+++ src/app.js
@@ -10,25 +10,47 @@ function buscar() {
       };
       // console.log(pos.lat);
       // obtener los datos de la api del tiempo en la posición dada
-      $.getJSON('https://api.darksky.net/forecast/dee89d5e9210fc4bb4366e0d5ccc2d0a/' + pos.lat + ',' + pos.lng + '?extend=daily&callback=?', function(forecast) {
+      $.getJSON('https://api.darksky.net/forecast/dee89d5e9210fc4bb4366e0d5ccc2d0a/' + pos.lat + ',' + pos.lng + '?extend=daily&callback=?&units=auto', function(forecast) {
         console.log(forecast);
         console.log(forecast.currently.temperature);
-        let fToCel = (forecast.currently.temperature - 32) * 5 / 9;
+        let fToCel = `${forecast.currently.temperature}°`;
         let temperature = $('<h2>').text(fToCel);
         let container = $('.main-container');
         let boton = $('.boton');
         let btn = $('<button type="button" class="btn btn-default">').text('Predicciones de la semana');
-        container.append(temperature);
+        let windSpeed = $('<p>').text(`${forecast.currently.windSpeed}  m/s`);
+        let humidity = $('<p>').text(`${forecast.currently.humidity}  %`);
+        let uvIndex = $('<p>').text(`${forecast.currently.uvIndex}`);
+        let pressure = $('<p>').text(`${forecast.currently.pressure}  hPa`);
+
+        container.append(temperature, windSpeed, humidity, uvIndex, pressure);
         boton.append(btn);
-
-
-
       });
     }, function(error) {
       alert('Tenemos un problema en encontrar tu ubicación');
     });
   }
 }
+
+var skycons = new Skycons({"color": "black"});
+  // on Android, a nasty hack is needed: {"resizeClear": true}
+
+  // you can add a canvas by it's ID...
+  skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+
+  // ...or by the canvas DOM element itself.
+  skycons.add(document.getElementById("icon2"), Skycons.RAIN);
+
+  // if you're using the Forecast API, you can also supply
+  // strings: "partly-cloudy-day" or "rain".
+
+  // start animation!
+  skycons.play();
+
+  // you can also halt animation with skycons.pause()
+
+  // want to change the icon? no problem:
+  skycons.set("icon1", Skycons.PARTLY_CLOUDY_NIGHT);
 
 // probando con node
 /* const fetch = require('node-fetch');
